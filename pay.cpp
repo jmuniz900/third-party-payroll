bff13fbd177db9f49b42895824ec0dcd09a730d3
Jose Muniz <jmuniz900@csu.fullerton.edu>
2019-02-28 17:19:02 -0800

    work on getCompanies()

--- pay.cpp
+++ pay.cpp
@@ -48,9 +48,21 @@ void readData(vector<Person> &employeeN){
 }
 
 void getCompanies(vector<Person> &employeeN, vector<string> &companyN){
+  int counter = 0;
+  companyN.push_back(employeeN[0].getCompanyName())
+
   for(int i = 0; i < employeeN.size(); i++){
-       companyN.push_back(employeeN[i].getCompanyName());
- }
+      // companyN.push_back(employeeN[i].getCompanyName());
+      while(counter < companyN.size()){
+        if(employeeN[i].getCompanyName() != companyN[counter]){
+          companyN.push_back(employeeN[i].getCompanyName())
+        }
+        counter++;
+      }
+    }
+    for(int j = 0; j < companyN.size(); j++){
+      cout << companyN[j] << endl;
+    }
 }
 void printHighestPaid(vector<Person> &employeeN){
   float tmp = 0;
